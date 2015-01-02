156206ea0193f17d360c6e199e3b9dd826c8b276
hajir <aminehajir@hotmail.fr>
2015-01-02 17:06:17 +0100

    création de la fonction faireJouer qui fait jouer un joueur

--- main.c
+++ main.c
@@ -10,13 +10,71 @@
 #include "menuDebut.h"
 
 
+void faireJouer(int *player)
+{
+	char piece;
+	int colonne;
+	booleen jouer; // variable qui reçoit vrai si le joueur a bien placer son pion, faux sinon
+	
+	jouer=FAUX;
+			
+	piece=menuChoixPion(joueurs[*player-1].nom);
+	while(!jouer) // tant que le joueur1 n'a pas placer son pion
+	{
+		colonne=menuChoixColonne(joueurs[*player-1].nom);
+		while(colonne==0 || colonne==8)
+		{
+			if(colonne==0)
+			{
+				piece=menuChoixPion(joueurs[*player-1].nom);
+				colonne=menuChoixColonne(joueurs[*player-1].nom);
+			}
+			else
+			{
+				if(colonne==8)
+				{
+					couleur(ROUGEGRAS);
+					printf("\n RAPPEL : %s, vous jouez avec des pions %s \n\n",joueurs[*player-1].nom,convert(joueurs[*player-1].couleur));
+					couleur(DEFAULT);
+					colonne=menuChoixColonne(joueurs[*player-1].nom);
+				}		
+			}
+		}
+			switch(piece)
+			{
+				case 'b' : 
+				{
+					jouer=placerBlocante(grille, hauteurColonne, colonne, *player,joueurs[*player-1].nom);
+				}break;
+					
+				case 'c' :
+				{
+					jouer=placerCreuse(grille, hauteurColonne, colonne, *player,joueurs[*player-1].nom);
+				}break;
+					
+				case 'p' : 
+				{
+					jouer=placerPleine(grille, hauteurColonne, colonne, *player,joueurs[*player-1].nom);
+				}break;
+			}
+	}
+	if( (*player) == 1 )
+	{
+		(*player)++; // quand le joueur1 aura bien placé son pion, on incrémente pour que le joueur2 puisse jouer à son tour
+	}
+	else
+	{
+		(*player)--; // on decrémente cette fois pour revenir au premier joueur
+	}
+}
+	
 int main()
 {
 	int i;
-	char piece;
-	int colonne;
+	//char piece;
+	//int colonne;
 	int player=1;  // variable qui permet d'alterner le tour des joueurs
-	booleen jouer; // variable qui reçoit vrai si le joueur a bien placer son pion, faux sinon
+	//booleen jouer; // variable qui reçoit vrai si le joueur a bien placer son pion, faux sinon
 	
 	printf("\n");
 	initialiser_jeu(grille);
@@ -32,102 +90,15 @@ int main()
 	{	
 		if(player==1)
 		{
-			jouer=FAUX;
-			
-			piece=menuChoixPion(joueurs[0].nom);
-			while(!jouer) // tant que le joueur1 n'a pas placer son jeton
-			{
-				colonne=menuChoixColonne(joueurs[0].nom);
-				while(colonne==0 || colonne==8)
-				{
-					if(colonne==0)
-					{
-						piece=menuChoixPion(joueurs[0].nom);
-						colonne=menuChoixColonne(joueurs[0].nom);
-					}
-					else
-					{
-						if(colonne==8)
-						{
-							couleur(ROUGEGRAS);
-							printf("\n RAPPEL : %s, vous jouez avec des pions %s \n\n",joueurs[0].nom,convert(joueurs[0].couleur));
-							couleur(DEFAULT);
-							colonne=menuChoixColonne(joueurs[0].nom);
-						}		
-					}
-				}
-				switch(piece)
-				{
-					case 'b' : 
-					{
-						jouer=placerBlocante(grille, hauteurColonne, colonne, player,joueurs[0].nom);
-					}break;
-					
-					case 'c' :
-					{
-						jouer=placerCreuse(grille, hauteurColonne, colonne, player,joueurs[0].nom);
-					}break;
-					
-					case 'p' : 
-					{
-						jouer=placerPleine(grille, hauteurColonne, colonne, player,joueurs[0].nom);
-						
-					}break;
-				}
-			}
-			player++; // quand le joueur1 aura bien placé son pion, on incrémente pour que le joueur2 puisse jouer à son tour
+			faireJouer(&player);
 		}
 		else
 		{
 			if(player==2)
 			{
-				jouer=FAUX;
-				
-				piece=menuChoixPion(joueurs[1].nom);
-				while(!jouer) // meme commentaire que précédemment
-				{
-					colonne=menuChoixColonne(joueurs[1].nom);
-					while(colonne==0 || colonne==8)
-					{
-						if(colonne==0)
-						{
-							piece=menuChoixPion(joueurs[1].nom);
-							colonne=menuChoixColonne(joueurs[1].nom);
-						}
-						else
-						{
-							if(colonne==8)
-							{
-								couleur(ROUGEGRAS);
-								printf("\n RAPPEL : %s, vous jouez avec des pions %s \n\n",joueurs[1].nom,convert(joueurs[1].couleur));
-								couleur(DEFAULT);
-								colonne=menuChoixColonne(joueurs[1].nom);
-							}		
-						}
-					}
-					switch(piece)
-					{
-						case 'b' : 
-						{
-							jouer=placerBlocante(grille, hauteurColonne, colonne, player,joueurs[1].nom);
-						}break;
-						
-						case 'c' :
-						{
-							jouer=placerCreuse(grille, hauteurColonne, colonne, player,joueurs[1].nom);
-						}break;
-						
-						case 'p' : 
-						{
-							jouer=placerPleine(grille, hauteurColonne, colonne, player,joueurs[1].nom);
-							
-						}break;
-					}
-				}
+				faireJouer(&player);
 			}
-			player--; // on decrémente cette fois pour revenir au premier joueur
 		}
-		
 	}while(grillePleine(grille,hauteurColonne) == FAUX);
 			
 	
