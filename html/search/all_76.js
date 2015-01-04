57dccbdf3a9382972f491c7cc568dca703b78d09
hajir <aminehajir@hotmail.fr>
2015-01-04 16:55:31 +0100

    mise à jour de generat° documtat° doxygen

--- html/search/all_76.js
+++ html/search/all_76.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['vertical_5fwin',['vertical_win',['../victoire_8c.html#a9e934806aafdbe7a9b3d97e927592c57',1,'vertical_win(pion T[N][M], int player, int ligne, int colonne):&#160;victoire.c'],['../victoire_8h.html#a9e934806aafdbe7a9b3d97e927592c57',1,'vertical_win(pion T[N][M], int player, int ligne, int colonne):&#160;victoire.c']]],
   ['victoire_2ec',['victoire.c',['../victoire_8c.html',1,'']]],
   ['victoire_2eh',['victoire.h',['../victoire_8h.html',1,'']]],
   ['vide',['VIDE',['../affichageJeu_8h.html#a3ee479371002a0a258e5c4111cc6e54f',1,'affichageJeu.h']]],
--- html/search/functions_76.js
+++ html/search/functions_76.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['vertical_5fwin',['vertical_win',['../victoire_8c.html#a9e934806aafdbe7a9b3d97e927592c57',1,'vertical_win(pion T[N][M], int player, int ligne, int colonne):&#160;victoire.c'],['../victoire_8h.html#a9e934806aafdbe7a9b3d97e927592c57',1,'vertical_win(pion T[N][M], int player, int ligne, int colonne):&#160;victoire.c']]],
   ['viderbuffer',['viderBuffer',['../boiteOutil_8c.html#a294587294111bfede1e11bdf876b6d10',1,'viderBuffer():&#160;boiteOutil.c'],['../boiteOutil_8h.html#a294587294111bfede1e11bdf876b6d10',1,'viderBuffer():&#160;boiteOutil.c']]]
 ];
