df39da73c9cb3bb12267dbccace42a4ebcd6b561
hajir <aminehajir@hotmail.fr>
2015-01-04 16:53:20 +0100

    ajout commentaire + documentat° pr fct° victoire verticale

--- victoire.c
+++ victoire.c
@@ -37,7 +37,7 @@ booleen case_du_joueur(pion T[N][M], int player, int ligne, int colonne)
 
 /**
  * \fn booleen horizontal_win(pion T[N][M], int player, int ligne, int colonne)
- * \brief fonction booleenne permettant de déceler un alignement horizontal de 4 pions du joueur en question
+ * \brief fonction booleenne permettant de déceler un alignement horizontal de 4 pions(ou plus) du joueur en question
  * \param pion la grille du jeu.
  * \param ligne indice de la ligne de la case de la matrice ou le joueur a placer son pion
  * \param colonne indice de la colonne de la case de la matrice ou le joueur a placer son pion
@@ -50,7 +50,7 @@ booleen horizontal_win(pion T[N][M], int player, int ligne, int colonne)
 	int cpt_droite=0; // compte le nombre de case voisines droites appartenant au joueur qui vient de placer son pion 
 	int cpt_gauche=0; // compte le nombre de case voisines gauches appartenant au joueur qui vient de placer son pion 
 	
-	// on cherche à droite s'il y a des cases qui appartiennent au joueur au question et on les dénombre 
+	// on cherche à droite s'il y a des cases qui appartiennent au joueur en question et on les dénombre 
 	while( colonne+i < M ) // tant qu'on est pas arrivé à la bordure droite de la grille
 	{
 		if( case_du_joueur(T,player, ligne, colonne+i) ) // si la case voisine appartient au joueur
@@ -78,7 +78,7 @@ booleen horizontal_win(pion T[N][M], int player, int ligne, int colonne)
 		}
 	}
 	if( (cpt_droite+cpt_gauche) >= 3 ) // si le nombre de voisine horizontale (gauche et droite) de la case ou le joueur a déposé son pion
-	{ 				    // est >=3 alors il y le joueur a gagné.
+	{ 				    // est >=3 alors le joueur a gagné.
 		return VRAI;
 	}
 	else // si inferieur à 3 alors il n'y a pas de victoire horizontale
@@ -87,6 +87,15 @@ booleen horizontal_win(pion T[N][M], int player, int ligne, int colonne)
 	}
 }
 
+/**
+ * \fn booleen vertical_win(pion T[N][M], int player, int ligne, int colonne)
+ * \brief fonction booleenne permettant de déceler un alignement vertical de 4 pions (ou plus) du joueur en question
+ * \param pion la grille du jeu.
+ * \param ligne indice de la ligne de la case de la matrice ou le joueur a placer son pion
+ * \param colonne indice de la colonne de la case de la matrice ou le joueur a placer son pion
+ * \return VRAI si une victoire verticale est detectée, FAUX sinon
+*/
+// meme principe que la fonction victoire horizontale, ici on parcourt les cases verticales au lieu des cases horizontales
 booleen vertical_win(pion T[N][M], int player, int ligne, int colonne)
 {
 	int i=1; // variable qui va parcourir les cases voisines qui se trouvent en dessous de la case ou le pion a été placé
@@ -94,25 +103,27 @@ booleen vertical_win(pion T[N][M], int player, int ligne, int colonne)
 	int cpt_bas=0; // compte le nombre de case voisines d'en dessous appartenant au joueur qui vient de placer son pion 
 	int cpt_haut=0; // compte le nombre de case voisines du dessus appartenant au joueur qui vient de placer son pion 
 	
-	while( ligne+i < N )
+	// on cherche en dessous s'il y a des cases qui appartiennent au joueur en question et on les dénombre 
+	while( ligne+i < N )// tant qu'on est pas atteint l'extrémité inférieure de la grille
 	{
-		if( case_du_joueur(T,player, ligne+i, colonne) )
+		if( case_du_joueur(T,player, ligne+i, colonne) ) // si la case voisine appartient au joueur 
 		{
-			cpt_bas++;
-			i++;
+			cpt_bas++; // on incrémente le compteur des cases voisines inférieures
+			i++; // et on va chercher si la case inférieure suivante appartient aussi au joueur
 		}
-		else
+		else // dès qu'on tombe sur une case qui n'appartient pas au joueur
 		{
-			i=N;
+			i=N; // on force la sortie de la boucle en affectant le nbre de lignes de la grille à i. 
+			     // on simule un depassement de la taille de la matrice (ligne+M > 5 qlq soit la valeur de ligne)
 		}
 	}
-	
-	while( ligne-j >= 0 ) // tant qu'on est pas arrivé à la bordure droite de la grille
+	// on cherche au dessus s'il y a des cases qui appartiennent au joueur en question et on les dénombre
+	while( ligne-j >= 0 ) // tant qu'on a pas atteint l'extremité supérieure de la grille
 	{
 		if( case_du_joueur(T,player, ligne-j, colonne) ) // si case appartient o joueur
 		{
-			cpt_haut++; // on incrémente le cpt de voisine gauche appartenant o meme joueur
-			j++;
+			cpt_haut++; // on incrémente le cpt des cases voisines supérieures appartenant o meme joueur
+			j++; // on verifie si la case supérieure suivante appartient également o joueur
 		}
 		else // sinon, on force la sortie de la boucle 
 		{
@@ -120,11 +131,12 @@ booleen vertical_win(pion T[N][M], int player, int ligne, int colonne)
 		}
 	}
 	
-	if( (cpt_bas+cpt_haut) >= 3 ) // si le nombre de voisine horizontale (gauche et droite) de la case ou le joueur a déposé son pion
-	{ 				    // est >=3 alors il y le joueur a gagné.
+	if( (cpt_bas+cpt_haut) >= 3 ) // si le nombre de cases voisines verticales (du dessus et d'en d'en dessous) de la case ou le joueur 
+				       // a déposé son pion est >=3 alors le joueur a gagné.
+	{ 				    
 		return VRAI;
 	}
-	else // si inferieur à 3 alors il n'y a pas de victoire horizontale
+	else // si inferieur à 3 alors il n'y a pas de victoire verticale
 	{
 		return FAUX;
 	}
